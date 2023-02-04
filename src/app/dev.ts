4744a0c29d46853bbbc39da950973df2c936eb22
Richard Solomou <richard@solomou.io>
2023-02-04 02:56:34 +0200

    fix options.db

--- src/app/dev.ts
+++ src/app/dev.ts
@@ -5,23 +5,48 @@ import * as YAML from 'yaml';
 
 import { StorageProvider, checkTarrasqueApp } from './common';
 
+/**
+ * Parse a boolean value from a string
+ * @param value - The value to parse
+ * @returns The parsed value
+ */
+function parseBoolean(value: string) {
+  if (value === 'true') {
+    return true;
+  } else if (value === 'false') {
+    return false;
+  } else {
+    throw new Error('Database service option can only be "true" or "false"');
+  }
+}
+
+/**
+ * Parse a storage provider value from a string
+ * @param value - The value to parse
+ * @returns The parsed value
+ */
+function parseStorageProvider(value: string) {
+  if (value === StorageProvider.Local) {
+    return StorageProvider.Local;
+  } else if (value === StorageProvider.S3) {
+    return StorageProvider.S3;
+  } else {
+    throw new Error(`Storage provider can only be "${StorageProvider.Local}" or "${StorageProvider.S3}"`);
+  }
+}
+
 export const dev = new Command('dev')
   .description('Run Tarrasque App in development mode')
-  .option('--db', 'Run the database service', true)
-  .option('--storage-provider <storageProvider>', 'Set the storage provider to use', StorageProvider.Local)
+  .option('--db [value]', 'Run the database service', parseBoolean, true)
+  .option(
+    '--storage-provider <storageProvider>',
+    'Set the storage provider to use',
+    parseStorageProvider,
+    StorageProvider.Local,
+  )
   .action(async (options) => {
     // Check the current working directory is the root of Tarrasque App
     checkTarrasqueApp();
-
-    // Database can only be true or false
-    if (typeof options.db !== 'boolean') {
-      throw new Error('Database service option can only be "true" or "false"');
-    }
-
-    // Storage provider can only be local or s3
-    if (options.storageProvider !== StorageProvider.Local && options.storageProvider !== StorageProvider.S3) {
-      throw new Error(`Storage provider can only be "${StorageProvider.Local}" or "${StorageProvider.S3}"`);
-    }
 
     // Get all services from the docker-compose.yaml file
     const dockerComposeYaml = await readFile('./docker-compose.yaml');
